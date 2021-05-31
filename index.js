836942de9dc1449b84054b5ec6a63dfa86e335de
Aveek Saha <aveek.s98@gmail.com>
2021-05-31 19:26:56 +0530

    Add icon color to obj

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
-const simpleIcons = require('simple-icons');
+const simpleIcons = require("simple-icons");
 
-const { hexToRGB, luminance } = require('./utils')
+const { hexToRGB, luminance } = require("./utils");
 
-const lumWhite = luminance(255,255,255)
-var data = []
+const lumWhite = luminance(255, 255, 255);
+var data = [];
 
 for (const title in simpleIcons) {
     const icon = simpleIcons.get(title);
+
+    const rgb = hexToRGB(icon.hex);
+    const lumBg = luminance(...rgb);
+    const ratio =
+        lumBg > lumWhite
+            ? (lumWhite + 0.05) / (lumBg + 0.05)
+            : (lumBg + 0.05) / (lumWhite + 0.05);
+
     const iconData = {
         title: icon.title,
         slug: icon.slug,
-        hex: icon.hex
-    }
-    data.push(iconData)
+        hex: icon.hex,
+        icon: ratio < 1 / 3 ? "ffffff" : "000000"
+    };
+    data.push(iconData);
 }
-var rgb = hexToRGB(data[1].hex)
-var lumBg = luminance(...rgb)
-const ratio = lumBg > lumWhite 
-    ? ((lumWhite + 0.05) / (lumBg + 0.05))
-    : ((lumBg + 0.05) / (lumWhite + 0.05));
-console.log(ratio < 1/3 ? 'ffffff' : '000000');
+
+console.log(data);
--- utils.js
+++ utils.js
@@ -1,19 +1,21 @@
 module.exports = {
-  hexToRGB(h) {
-    let r = 0,
-      g = 0,
-      b = 0;
-    r = parseInt(h[0] + h[1], 16);
-    g = parseInt(h[2] + h[3], 16);
-    b = parseInt(h[4] + h[5], 16);
+    hexToRGB(h) {
+        let r = 0,
+            g = 0,
+            b = 0;
+        r = parseInt(h[0] + h[1], 16);
+        g = parseInt(h[2] + h[3], 16);
+        b = parseInt(h[4] + h[5], 16);
 
-    return [r, g, b];
-  },
-  luminance(r, g, b) {
-    var a = [r, g, b].map(function (v) {
-      v /= 255;
-      return v <= 0.03928 ? v / 12.92 : Math.pow((v + 0.055) / 1.055, 2.4);
-    });
-    return a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722;
-  },
+        return [r, g, b];
+    },
+    luminance(r, g, b) {
+        var a = [r, g, b].map(function (v) {
+            v /= 255;
+            return v <= 0.03928
+                ? v / 12.92
+                : Math.pow((v + 0.055) / 1.055, 2.4);
+        });
+        return a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722;
+    },
 };
