817543af5435afa58a5752c2b4e31bede03d44f8
Aveek Saha <aveek.s98@gmail.com>
2021-06-01 20:18:03 +0530

    Generate markdown table

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# GitHub-Profile-Badges
+# GitHub Profile sBadges
 Clean badges for your GitHub Profile Readme
+
+<!-- Please do not modify this auto generated content -->
+<!-- AUTO-GENERATED-CONTENT:START (BADGES) -->
+
+<!-- AUTO-GENERATED-CONTENT:END -->
--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const simpleIcons = require("simple-icons");
-
+const path = require("path");
+const table = require("markdown-table");
+const markdownMagic = require("markdown-magic");
 const { hexToRGB, luminance } = require("./utils");
 
 const lumWhite = luminance(255, 255, 255);
@@ -15,13 +17,38 @@ for (const title in simpleIcons) {
             ? (lumWhite + 0.05) / (lumBg + 0.05)
             : (lumBg + 0.05) / (lumWhite + 0.05);
 
-    const iconData = {
-        title: icon.title,
-        slug: icon.slug,
-        hex: icon.hex,
-        icon: ratio < 1 / 3 ? "ffffff" : "000000"
-    };
+    // const iconData = {
+    //     title: icon.title,
+    //     slug: icon.slug,
+    //     hex: icon.hex,
+    //     icon: ratio < 1 / 3 ? "ffffff" : "000000"
+    // };
+
+    const iconData = [
+        icon.title,
+        `	<img alt="Python" src="https://img.shields.io/badge/${icon.title}-${
+            icon.hex
+        }.svg?style=for-the-badge&logo=${icon.slug}&logoColor=${
+            ratio < 1 / 3 ? "ffffff" : "000000"
+        }"/>`,
+    ];
     data.push(iconData);
 }
+
+function generate(data) {
+    const config = {
+        transforms: {
+            BADGES() {
+                return table([["Name", "Badge"], ...data]);
+            },
+        },
+    };
+
+    markdownMagic(path.join(__dirname, "README.md"), config, (d) => {
+        console.log(`Added badges`);
+    });
+}
+
+generate(data)
 
 console.log(data);
