4ae96cf65b901c51ec6b592da211fb724b84c328
Aveek Saha <aveek.s98@gmail.com>
2021-06-07 00:37:39 +0530

    Copy to clipboard on click

--- src/src/App.svelte
+++ src/src/App.svelte
@@ -1,5 +1,6 @@
 <script>
     export let icons;
+	new ClipboardJS('.card');
 </script>
 
 <main>
@@ -21,19 +22,16 @@
         <div class="row row-cols-6">
             {#each Object.entries(icons) as [name]}
                 <div class="col mb-3">
-                    <div class="card h-100">
+                    <div class="card h-100" data-clipboard-text="{icons[name].src}">
                         <div class="img-badge">
                             <img
                                 src={icons[name].src}
                                 class="card-img-top"
                                 alt={name}
                             />
-						</div>
+                        </div>
                         <div class="card-body">
                             <p class="card-text text-center">{name}</p>
-                            <!-- <a href="#" class="btn btn-primary stretched-link"
-                            >Go somewhere</a
-                        > -->
                         </div>
                     </div>
                 </div>
